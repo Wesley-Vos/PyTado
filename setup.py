697a65bf0614559aa7467f7db666c63ee06dacac
Jørgen Vatle <Jorgen@WcAServices.net>
2022-07-03 15:42:34 +0300

    Bump development version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.argv[-1] == 'publish':
 readme = open(here('README.md')).read()
 
 setup(name='jorgenvatle-python-tado',
-      version='0.14.dev1',
+      version='0.14.dev2',
       description='PyTado from chrism0dwk, modfied by w.malgadey, diplix, michaelarnauts, LenhartStephan, splifter, syssi, andersonshatch, Yippy, p0thi',
       long_description=readme,
       keywords='tado',
